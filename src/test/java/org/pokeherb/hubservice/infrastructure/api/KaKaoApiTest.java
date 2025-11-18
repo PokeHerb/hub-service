2515a6e68724482b949ff96a456527d89ec58833
jinnk0 <jin8073@gmail.com>
2025-11-18 15:42:12 +0900

    test: 허브 주소 입력 시 위도, 경도 변환 기능 테스트 (#3) - Kakao Map API 연동 테스트 포함

---
+++ src/test/java/org/pokeherb/hubservice/infrastructure/api/KaKaoApiTest.java
@@ -0,0 +1,23 @@
+package org.pokeherb.hubservice.infrastructure.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.pokeherb.hubservice.domain.hub.service.AddressToCoordinateConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class KaKaoApiTest {
+
+    @Autowired
+    AddressToCoordinateConverter converter;
+
+    @Test
+    @DisplayName(value = "카카오 API 주소 좌표 변환 테스트")
+    void kakaoApiTest() {
+        List<Double> coordinate = converter.convert("서울특별시 송파구 송파대로 55");
+        System.out.println(coordinate);
+    }
+}
