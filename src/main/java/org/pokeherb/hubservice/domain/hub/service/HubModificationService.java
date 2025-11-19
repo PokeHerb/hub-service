94a303588309d0f1e32821244b832e8309bd0964
jinnk0 <jin8073@gmail.com>
2025-11-19 16:59:35 +0900

    refactor: 허브 삭제, 수정 로직 위치 변경

--- src/main/java/org/pokeherb/hubservice/domain/hub/service/HubModificationService.java
+++
@@ -1,7 +0,0 @@
-package org.pokeherb.hubservice.domain.hub.service;
-
-import org.pokeherb.hubservice.domain.hub.entity.Hub;
-
-public interface HubModificationService {
-    Hub modifyHubAddress(Hub hub, String sido, String sigungu, String eupmyeon, String dong, String ri, String street, String building_no, String details);
-}
