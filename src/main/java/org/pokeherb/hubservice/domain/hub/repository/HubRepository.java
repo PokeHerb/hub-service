1867915f98e9cb63df5cb30a4d3a6ad435d4d0be
jinnk0 <jin8073@gmail.com>
2025-11-19 15:32:20 +0900

    feat: hub 조회를 위한 query 모델 구현 (#12)

--- src/main/java/org/pokeherb/hubservice/domain/hub/repository/HubRepository.java
+++ src/main/java/org/pokeherb/hubservice/domain/hub/repository/HubRepository.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.SQLRestriction;
 import org.pokeherb.hubservice.domain.hub.entity.Hub;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HubRepository extends JpaRepository<Hub, Long> {
@@ -12,4 +13,6 @@ public interface HubRepository extends JpaRepository<Hub, Long> {
 
     @SQLRestriction("deleted_at is null")
     Optional<Hub> findByHubName(String hubName);
+
+    List<Hub> findAllByDeletedAtIsNull();
 }
