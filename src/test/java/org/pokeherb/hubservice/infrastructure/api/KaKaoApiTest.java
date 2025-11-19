5a2b1f2753bafd728bee803adad0b88fddda9c6e
jinnk0 <jin8073@gmail.com>
2025-11-19 09:41:00 +0900

    test: 카카오 API를 활용하여 허브 간 이동거리 및 소요시간 계산 기능 테스트 (#6)

--- src/test/java/org/pokeherb/hubservice/infrastructure/api/KaKaoApiTest.java
+++ src/test/java/org/pokeherb/hubservice/infrastructure/api/KaKaoApiTest.java
@@ -1,13 +1,20 @@
 package org.pokeherb.hubservice.infrastructure.api;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.pokeherb.hubservice.domain.hub.entity.Hub;
+import org.pokeherb.hubservice.domain.hub.repository.HubRepository;
 import org.pokeherb.hubservice.domain.hub.service.AddressToCoordinateConverter;
+import org.pokeherb.hubservice.domain.hub.service.CheckAccessHub;
+import org.pokeherb.hubservice.domain.hubroute.service.TravelInfoCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.UUID;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,10 +23,61 @@ public class KaKaoApiTest {
     @Autowired
     AddressToCoordinateConverter converter;
 
+    @Autowired
+    TravelInfoCalculator calculator;
+
+    @Autowired
+    HubRepository hubRepository;
+
+    @Autowired
+    CheckAccessHub checkAccessHub;
+
+    @Autowired
+    AddressToCoordinateConverter addressConverter;
+
+    private UUID startHubId;
+    private UUID endHubId;
+
+    @BeforeEach
+    void setup() {
+        Hub startHub = Hub.builder()
+                .sido("서울특별시")
+                .sigungu("송파구")
+                .street("송파대로")
+                .building_no("55")
+                .checkAccessHub(checkAccessHub)
+                .converter(addressConverter)
+                .build();
+
+        Hub endHub = Hub.builder()
+                .sido("경기도")
+                .sigungu("고양시 덕양구")
+                .street("권율대로")
+                .building_no("570")
+                .checkAccessHub(checkAccessHub)
+                .converter(addressConverter)
+                .build();
+
+        hubRepository.save(startHub);
+        hubRepository.save(endHub);
+
+        startHubId = startHub.getHubId();
+        endHubId = endHub.getHubId();
+    }
+
     @Test
     @DisplayName(value = "카카오 API 주소 좌표 변환 테스트")
-    void kakaoApiTest() {
+    void kakaoApiGeoCodingTest() {
         List<Double> coordinate = converter.convert("서울특별시 송파구 송파대로 55");
         System.out.println(coordinate);
     }
+
+    @Test
+    @DisplayName(value = "카카오 API 경로 계산 테스트")
+    void kakaoApiNaviTest() {
+
+        List<Double> travelInfo = calculator.calculateTravelInfo(startHubId, endHubId);
+        System.out.println(travelInfo);
+    }
+
 }
