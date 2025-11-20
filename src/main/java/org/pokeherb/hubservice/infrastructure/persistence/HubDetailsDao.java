c8142c93ddf63b8404ffca6f17524a29d811377a
jinnk0 <jin8073@gmail.com>
2025-11-20 12:56:30 +0900

    add: Querydsl을 활용하여 검색 키워드를 포함한 허브 리스트 반환 메소드 구현 (#13)

--- src/main/java/org/pokeherb/hubservice/infrastructure/persistence/HubDetailsDao.java
+++ src/main/java/org/pokeherb/hubservice/infrastructure/persistence/HubDetailsDao.java
@@ -2,12 +2,30 @@ package org.pokeherb.hubservice.infrastructure.persistence;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.pokeherb.hubservice.domain.hub.entity.Hub;
+import org.pokeherb.hubservice.domain.hub.entity.QHub;
 import org.pokeherb.hubservice.domain.hub.repository.HubDetailsRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class HubDetailsDao implements HubDetailsRepository {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<Hub> searchHubByKeyword(String keyword) {
+        QHub hub = QHub.hub;
+        return jpaQueryFactory.selectFrom(hub)
+                .where(hub.hubName.containsIgnoreCase(keyword)
+                        .or(hub.address.sido.containsIgnoreCase(keyword))
+                        .or(hub.address.sigungu.containsIgnoreCase(keyword))
+                        .or(hub.address.eupmyeon.containsIgnoreCase(keyword))
+                        .or(hub.address.ri.containsIgnoreCase(keyword))
+                        .or(hub.address.dong.containsIgnoreCase(keyword))
+                        .or(hub.address.street.containsIgnoreCase(keyword)))
+                .fetch();
+    }
 }
