19726a6e63f7e67ba18a3550cf0898b92a9e2eea
jinnk0 <jin8073@gmail.com>
2025-11-21 16:29:53 +0900

    feat: 최종 이동 경로 캐싱 구현 (#15) - 최종 이동 경로 조회 시 해당 정보 캐싱 및 조회 결과를 바탕으로 집합 인덱스 키 생성

--- src/main/java/org/pokeherb/hubservice/application/hubroute/query/FinalHubRouteQueryServiceImpl.java
+++ src/main/java/org/pokeherb/hubservice/application/hubroute/query/FinalHubRouteQueryServiceImpl.java
@@ -1,10 +1,12 @@
 package org.pokeherb.hubservice.application.hubroute.query;
 
 import lombok.RequiredArgsConstructor;
+import org.pokeherb.hubservice.application.cache.CacheService;
 import org.pokeherb.hubservice.application.hub.dto.HubResponse;
 import org.pokeherb.hubservice.domain.hub.entity.Hub;
 import org.pokeherb.hubservice.domain.hub.repository.HubRepository;
 import org.pokeherb.hubservice.domain.hubroute.service.FinalRouteFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +20,19 @@ public class FinalHubRouteQueryServiceImpl implements FinalHubRouteQueryService 
 
     private final FinalRouteFactory finalRouteFactory;
     private final HubRepository hubRepository;
+    private final CacheService cacheService;
 
     @Override
+    @Cacheable(
+            value = "finalRouteCache",
+            key = "T(String).valueOf(#startHubId) + '::' + T(String).valueOf(#endHubId)"
+    )
     public List<HubResponse> getFinalHubRoute(Long startHubId, Long endHubId) {
         List<Long> routeSequence = finalRouteFactory.getRouteSequence(startHubId, endHubId);
+        String routeKey = "finalRouteCache::" + startHubId + "::" + endHubId;
+
+        // 최종 경로 캐시 인덱스 생성
+        cacheService.putFinalRoute(routeSequence, routeKey);
 
         List<Hub> hubs = hubRepository.findByHubIdInAndDeletedAtIsNull(routeSequence);
         Map<Long, Hub> hubMap = hubs.stream()
