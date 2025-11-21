0592d35dcc379c7cfe20595c2c831026a90cea54
jinnk0 <jin8073@gmail.com>
2025-11-21 10:28:51 +0900

    feat: checkAccessHub 인터페이스 구현 (#5) - SecurityContextHolder의 정보를 활용하여 checkAccessHub 구현 - 마스터 권한을 보유하고 있지 않을 경우 접근 차단

--- src/main/java/org/pokeherb/hubservice/infrastructure/security/SecurityCheckAccessHub.java
+++ src/main/java/org/pokeherb/hubservice/infrastructure/security/SecurityCheckAccessHub.java
@@ -1,12 +1,24 @@
 package org.pokeherb.hubservice.infrastructure.security;
 
 import org.pokeherb.hubservice.domain.hub.service.CheckAccessHub;
+import org.pokeherb.hubservice.global.infrastructure.error.GeneralErrorCode;
+import org.pokeherb.hubservice.global.infrastructure.exception.CustomException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityCheckAccessHub implements CheckAccessHub {
     @Override
     public void checkAccess() {
-        // TODO: 현재 로그인 유저의 권한 (ROLE)이 마스터 관리자인지 확인
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        boolean isPermitted = false;
+        if (auth != null && auth.getPrincipal() instanceof UserDetails userDetails) {
+            isPermitted = userDetails.getAuthorities().stream().anyMatch(authority -> authority.getAuthority().equals("ROLE_MASTER"));
+        }
+        if (!isPermitted) {
+            throw new CustomException(GeneralErrorCode.FORBIDDEN_403);
+        }
     }
 }
