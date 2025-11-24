87cc01ea9204386e2e11d91221610a985e593937
jinnk0 <jin8073@gmail.com>
2025-11-24 13:51:20 +0900

    feat: 최종 경로 계산 시 cost를 인자로 받아 결정 (#21) - pgRouting으로 최종 경로 계산 시 cost로 사용할 컬럼명을 인자로 받아 결정하도록 수정 - cost에 입력한 컬럼명이 올바르지 않을 경우 발생하는 에러 코드 작성 - cost에 들어갈 수 있는 컬럼명의 화이트리스트를 작성하여 허용된 컬럼명만 사용할 수 있도록 구현

--- src/main/java/org/pokeherb/hubservice/domain/hubroute/exception/HubRouteErrorCode.java
+++ src/main/java/org/pokeherb/hubservice/domain/hubroute/exception/HubRouteErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum HubRouteErrorCode implements BaseErrorCode {
-    HUB_ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "HUB_ROUTE404", "해당 허브 간 이동 정보가 존재하지 않습니다.");
+    HUB_ROUTE_NOT_FOUND(HttpStatus.NOT_FOUND, "HUB_ROUTE404", "해당 허브 간 이동 정보가 존재하지 않습니다."),
+    INVALID_COST_TYPE(HttpStatus.BAD_REQUEST, "COST_INVALID400", "허브 경로 계산을 위한 cost 타입이 유효하지 않습니다. cost 타입은 duration과 distance만 가능합니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/org/pokeherb/hubservice/infrastructure/persistence/PgroutingFinalRouteFactory.java
+++ src/main/java/org/pokeherb/hubservice/infrastructure/persistence/PgroutingFinalRouteFactory.java
@@ -1,11 +1,14 @@
 package org.pokeherb.hubservice.infrastructure.persistence;
 
 import lombok.RequiredArgsConstructor;
+import org.pokeherb.hubservice.domain.hubroute.exception.HubRouteErrorCode;
 import org.pokeherb.hubservice.domain.hubroute.service.FinalRouteFactory;
+import org.pokeherb.hubservice.global.infrastructure.exception.CustomException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 @RequiredArgsConstructor
@@ -13,22 +16,33 @@ public class PgroutingFinalRouteFactory implements FinalRouteFactory {
 
     private final JdbcTemplate jdbcTemplate;
 
+    private static final Map<String, String> COST_COLUMN_MAP = Map.of(
+            "duration", "duration",
+            "distance", "distance"
+    );
+
     @Override
-    public List<Long> getRouteSequence(Long startHubId, Long endHubId) {
-        String sql = """
+    public List<Long> getRouteSequence(Long startHubId, Long endHubId, String cost) {
+
+        String costType = COST_COLUMN_MAP.get(cost);
+        if (costType == null) {
+            throw new CustomException(HubRouteErrorCode.INVALID_COST_TYPE);
+        }
+
+        String sql = String.format("""
                 SELECT route.node
                 FROM pgr_dijkstra(
                     'SELECT hub_route_id AS id,
                             start_hub_id AS source,
                             end_hub_id AS target,
-                            duration AS cost
+                            %s AS cost
                     FROM p_hub_route
                     WHERE deleted_at IS NULL',
                     ?, ?,
                     directed := true
                 ) AS route
                 ORDER BY route.seq;
-                """;
+                """, COST_COLUMN_MAP.get(cost));
         return jdbcTemplate.query(
                 sql,
                 (rs, rowNum) -> rs.getLong("node"),
