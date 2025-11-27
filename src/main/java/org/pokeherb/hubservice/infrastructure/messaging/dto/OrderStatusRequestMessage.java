250b1c224564e719d5cb1f1cb15f06097889b216
jinnk0 <jin8073@gmail.com>
2025-11-27 13:28:43 +0900

    fix: orderStatus 변경 요청 메시지를 발행할 때 변경 시점을 포함하여 전송 (#22)

--- src/main/java/org/pokeherb/hubservice/infrastructure/messaging/dto/OrderStatusRequestMessage.java
+++ src/main/java/org/pokeherb/hubservice/infrastructure/messaging/dto/OrderStatusRequestMessage.java
@@ -1,9 +1,11 @@
 package org.pokeherb.hubservice.infrastructure.messaging.dto;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record OrderStatusRequestMessage(
         UUID orderId,
-        String orderStatus
+        String orderStatus,
+        LocalDateTime changedAt
 ) {
 }
